7af18569e7f94b14e0c5cc9cb777f4ccd680858d
indicozy <berkutkarlibai@gmail.com>
2023-11-05 14:37:20 +0600

    k

--- scripts/getEnv.ts
+++ scripts/getEnv.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import z from "zod";
 
 export const getDotenv = () => {
-  dotenv.config(); // Load environment variables from .env file
+  const lol = dotenv.config({ path: __dirname + "/../.env" }); // Load environment variables from .env file
   const zEnv = z.object({
     R2_ACCESS_KEY_ID: z.string(),
     R2_SECRET_ACCESS_KEY: z.string(),
